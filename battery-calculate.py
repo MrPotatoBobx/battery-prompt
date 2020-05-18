454fa3d58460e0cc417baab62b08dc74c84e90bc
MrPotatoBob <potatothathatestomatos@gmail.com>
2020-05-18 19:20:54 +0300

    fixed prompt not showing
    made it so that it also updates the number in the while loop

--- battery-calculate.py
+++ battery-calculate.py
@@ -7,6 +7,12 @@ percentnum = percent.split()
 percent4real = percentnum[3]
 promptnum = "10%"
 while percent4real != promptnum:
+    battery = open("output.txt", "r")
+    percent = battery.readline()
+    percentnum = percent.split()
+    percent4real = percentnum[3]
+    promptnum = "10%"
+
     os.system("acpi -V > ~/battery-prompt/output.txt")
     time.sleep(30)
 os.system("python3 ~/battery-prompt/gui.py")

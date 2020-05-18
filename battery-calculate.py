a9e8acd0a950e97e0b35966c9269c91dcb1e0e1f
MrPotatoBob <potatothathatestomatos@gmail.com>
2020-05-18 19:18:59 +0300

    fixed  it not workig
    fixed it so it can upate the while loop

--- battery-calculate.py
+++ battery-calculate.py
@@ -8,7 +8,13 @@ percentnum = percent.split()
 percent4real = percentnum[3]
 promptnum = "10%"
 while percent4real != promptnum:
+    
+    battery = open("output.txt", "r")
+    percent = battery.readline()
+    percentnum = percent.split()
+    percent4real = percentnum[3]
+    promptnum = "10%"
     os.system("acpi -V > output.txt")
-    time.sleep(30)
+    time.sleep(5)
 os.system("python3 gui.py")
 

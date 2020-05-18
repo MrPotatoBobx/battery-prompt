e2ad4e5b04f1f7c0dd042672f077b3f7564c711d
MrPotatoBob <potatothathatestomatos@gmail.com>
2020-05-18 17:49:06 +0300

    lots of work
    added setup.py (for configureing features in the future) and fixed a directory issue

--- battery-calculate.py
+++ battery-calculate.py
@@ -1,12 +1,14 @@
 import os
 import time
-os.system("acpi -V > ~/battery-prompt/output.txt")
+
+os.system("acpi -V > output.txt")
 battery = open("output.txt", "r")
 percent = battery.readline()
 percentnum = percent.split()
 percent4real = percentnum[3]
 promptnum = "10%"
 while percent4real != promptnum:
-    os.system("acpi -V > ~/battery-prompt/output.txt")
+    os.system("acpi -V > output.txt")
     time.sleep(30)
-os.system("python3 ~/battery-prompt/gui.py")
+os.system("python3 gui.py")
+
---
+++ setup.py
@@ -0,0 +1,3 @@
+guidir = input("Enter the directory of gui.py:  ")
+batcaldir = input ("Enter the directory of battery-calculate.py: ")
+outdir = input ("Enter the directory of output.txt: ")

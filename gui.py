d4b6e0430d147aff060e13712a0499ded9c162d4
MrPotatoBob <potatothathatestomatos@gmail.com>
2020-05-19 14:51:31 +0300

    fixed checkbox
    it does not take it by 50% but it should help with battery

--- gui.py
+++ gui.py
@@ -13,25 +13,29 @@ class MainWindow(Gtk.Window):
         self.label = Gtk.Label("Your laptop's battery is 10%, please plug it in.")
         vbox.pack_start(self.label, True, True, 0)
         self.checkb = Gtk.CheckButton("Change brightness to 50%")
-        self.checkb.connect("clicked", self.closewindowb)
+        self.checkb.set_active(False)
 
 
         vbox.pack_start(self.checkb, True, True, 0)
         self.button = Gtk.Button(label="Ok")
-        self.button.connect("on", self.closewindow)
+        self.button.connect("clicked", self.closewindow)
         vbox.pack_start(self.button, True, True, 0)
     
     def closewindow(self, widget):
-        exit()
-    def closewindowb(self, widget):
+        if self.checkb.get_active():
+            os.system("xdotool key XF86MonBrightnessDown")
+            os.system("xdotool key XF86MonBrightnessDown")
+            os.system("xdotool key XF86MonBrightnessDown")
+            os.system("xdotool key XF86MonBrightnessDown")
+            os.system("xdotool key XF86MonBrightnessDown")
+            os.system("xdotool key XF86MonBrightnessDown")
+            exit()
+        else:
+            exit()
+   
        
-        os.system("xdotool key XF86MonBrightnessDown")
-        os.system("xdotool key XF86MonBrightnessDown")
-        os.system("xdotool key XF86MonBrightnessDown")
-        os.system("xdotool key XF86MonBrightnessDown")
            
         
-
 window = MainWindow()
 window.connect("delete-event", Gtk.main_quit)
 window.show_all()

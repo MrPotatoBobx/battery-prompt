e6343b0ae21b47a53d8087d10153f2c7c672b7fb
MrPotatoBob <potatothathatestomatos@gmail.com>
2020-05-19 15:37:23 +0300

    added  2 new features
    made it so it checks if the battery is charging and it will prompt if the battery is 10% or lower and will prompt if your battery is lower than 10 percent when you unplug

--- battery-calculate.py
+++ battery-calculate.py
@@ -7,17 +7,29 @@ percent = battery.readline()
 percentnum = percent.split()
 percent4real = percentnum[3]
 percentage = int(percent4real[:-1])
-while percentage <= 10:
-    
+charge = percentnum[2]
+
+while charge != "Discharging,":
     battery = open("output.txt", "r")
-   
-    
     percent = battery.readline()
     percentnum = percent.split()
     percent4real = percentnum[3]
     percentage = int(percent4real[:-1])
-    print ("test")
+    charge = percentnum[2]  
     os.system("acpi -V > output.txt")
+    time.sleep(10)
+
+
+
+while percentage >= 10:
+    
+    battery = open("output.txt", "r")
+   
+    os.system("acpi -V > output.txt")
+    percent = battery.readline()
+    percentnum = percent.split()
+    percent4real = percentnum[3]
+    percentage = int(percent4real[:-1])
     time.sleep(10)
 os.system("python3 gui.py")
 
